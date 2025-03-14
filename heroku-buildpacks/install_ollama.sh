ede73ce23b36b569762e459a38818006774cf1da
roalibiten <46627141+roalibiten@users.noreply.github.com>
2025-03-13 23:22:31 -0700

    Testing simple buildpack

--- heroku-buildpacks/install_ollama.sh
+++ heroku-buildpacks/install_ollama.sh
@@ -1,15 +1,1 @@
-#!/bin/bash
-
-# Ensure that curl is installed (if not, install it)
-apt-get update -y && apt-get install -y curl
-
-# Download and install Ollama
-curl -fsSL https://ollama.com/install.sh | sh
-
-# Check if the installation was successful
-if command -v ollama &>/dev/null; then
-    echo "Ollama installed successfully"
-else
-    echo "Ollama installation failed"
-    exit 1
-fi
+echo "Hello from custom buildpack"
